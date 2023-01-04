17c4a151e12773afd66c4107cc164259be66c8d5
Vladyslav.Plietnov <vladyslav_plietnov@epam.com>
2023-01-04 11:53:46 +0200

    Passing arguments to an agent

--- src/main/java/ua/mstt/plietnov/BookBuyerAgent.java
+++ src/main/java/ua/mstt/plietnov/BookBuyerAgent.java
@@ -1,15 +1,38 @@
 package ua.mstt.plietnov;
 
 
+import jade.core.AID;
 import jade.core.Agent;
 
 /**
- *  @author Vladyslav Plietnov
+ * @author Vladyslav Plietnov
  */
 
 public class BookBuyerAgent extends Agent {
+    // The title of the book to buy
+    private String targetBookTitle;
+    // The list of known seller agents
+    private AID[] sellerAgents = {new AID("seller1", AID.ISLOCALNAME),
+            new AID("seller2", AID.ISLOCALNAME)};
+
     protected void setup() {
         // Printout a welcome message
         System.out.println("Hello !Buyer - agent " + getAID().getName() + "is ready.");
+
+        Object[] args = getArguments();
+        if (args != null && args.length > 0) {
+            targetBookTitle = (String) args[0];
+            System.out.println("Trying to buy " + targetBookTitle);
+        } else {
+            // Make the agent terminate immediately
+            System.out.println("No book title specified");
+            doDelete();
+        }
+    }
+
+    // Put agent clean-up operations here
+    protected void takeDown() {
+        // Printout a dismissal message
+        System.out.println("Buyer-agent " + getAID().getName() + " terminating.");
     }
 }
